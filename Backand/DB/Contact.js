628b5cc4e26f381cb12a8291848cc38a55ad4725
Asha021 <apal66058@gmail.com>
2025-07-01 13:45:26 +0530

    initilise code

--- Backand/DB/Contact.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const schemaData = new mongoose.Schema({
-        name:String,
-        msg:String,
-        pn:Number,
-        
-})
-module.exports = mongoose.model("contacts",schemaData);
